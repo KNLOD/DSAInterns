29b6786750c174d5b1a4946b921352f2d24d5b4d
GitWhizKid <forvoxchannel@gmail.com>
2024-03-14 21:38:24 +0300

    Realization of ArrayList

--- Program.cs
+++ Program.cs
@@ -31,12 +31,12 @@ public class ArrayList<T> : IEnumerable<T>
 
     public ArrayList(IEnumerable<T> collection)
     {
-        int capacity = 0;
+        int _capacity = 0;
         foreach (T item in collection) 
         {
-            capacity++;
+            _capacity++;
         }
-        Capacity = capacity;
+        Capacity = _capacity;
 
         array = new T[Capacity];
         
@@ -44,8 +44,143 @@ public class ArrayList<T> : IEnumerable<T>
         foreach (T item in collection)
         {
             array[i++] = item;
+            Count++;
         }
     }
 
+    public void Add(T item)
+    {
+        if (Count == Capacity)
+        {
+            Resize(Capacity * 2);
+        }
+        array[Count++] = item;
+    }
+
+    public void AddRange(IEnumerable<T> collection)
+    {
+        foreach (T item in collection)
+        {
+            Add(item);
+        }
+    }
+
+    public void Insert(int index, T item)
+    {
+        if (index < 0 || index > Count)
+            throw new ArgumentOutOfRangeException("index");
+
+        if (Count == Capacity)
+        {
+            Resize(Capacity * 2);
+        }
+
+        Array.Copy(array, index, array, index + 1, Count - index);
+        array[index] = item;
+        Count++;
+    }
+
+    public void InsertRange(int index, IEnumerable<T> collection)
+    {
+        foreach (T item in collection)
+        {
+            Insert(index++, item);
+        }
+    }
+
+    public bool Remove(T item)
+    {
+        int _index = IndexOf(item);
+        if (_index >= 0)
+        {
+            RemoveAt(_index);
+            return true;
+        }
+        return false;
+    }
+
+    public void RemoveRange(int index, int count)
+    {
+        if (index < 0 || index + count > Count)
+            throw new ArgumentOutOfRangeException("index");
+
+        Array.Copy(array, index + count, array, index, Count - index - count);
+        Count -= count;
+    }
+
+    public void RemoveAt(int index)
+    {
+        if (index < 0 || index >= Count)
+            throw new ArgumentOutOfRangeException("index");
+
+        Array.Copy(array, index + 1, array, index, Count - index - 1);
+        Count--;
+    }
+
+    public int IndexOf(T item)
+    {
+        for (int i = 0; i < Count; i++)
+        {
+            if (EqualityComparer<T>.Default.Equals(array[i], item))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int LastIndexOf(T item)
+    {
+        for (int i = Count - 1; i >= 0; i--)
+        {
+            if (EqualityComparer<T>.Default.Equals(array[i], item))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public bool Contains(T item)
+    {
+        return IndexOf(item) >= 0;
+    }
+
+    public void Clear()
+    {
+        Array.Clear(array, 0, Count);
+        Count = 0;
+    }
+
+    public T[] ToArray()
+    {
+        T[] result = new T[Count];
+        Array.Copy(array, result, Count);
+        return result;
+    }
+
+    public void CopyTo(T[] destArray, int arrayIndex)
+    {
+        Array.Copy(array, 0, destArray, arrayIndex, Count);
+    }
+
+    private void Resize(int newCapacity)
+    {
+        T[] newArray = new T[newCapacity];
+        Array.Copy(array, newArray, Count);
+        array = newArray;
+        Capacity = newCapacity;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 0; i < Count; i++)
+            yield return array[i];
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
 } 
 
